2b0322667d4e455624da7eadca67feb975106a2b
Denis Kruglenko <denisvkr@gmail.com>
2017-08-09 18:12:49 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+/*A script with two variables, each assigned to a number. 
+Add them together and output the result to the console. 
+Now do the same with two strings.*/
+
+var num1 = 2;
+var num2 = 5;
+console.log (num1 + num2);
+
+var string1 = "Hey, how are you ";
+var string2 = " "
+
