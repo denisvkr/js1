b275b6b746af4b5bbc805ddf26145277780daa4e
Denis Kruglenko <denisvkr@gmail.com>
2017-08-09 19:17:16 -0400

    removed js file

--- main.js
+++ main.js
@@ -7,5 +7,56 @@ var num2 = 5;
 console.log (num1 + num2);
 
 var string1 = "Hey, how are you ";
-var string2 = " "
+var string2 = "doing?";
+console.log (string1 + string2);
+
+/* Create a multidimensional array related to a subject that interests you. 
+Output two of the items in sub-arrays to the console.*/
+
+var color = ["red", "green", "blue", "pink"];
+var tag = ["p", "h1", "span", "div"];
+var mix = [color, tag];
+
+console.log (mix[0][1], mix[1][3]);
+
+// more than 100 script 
+
+var num = 20;
+if (num <= 100) {
+	alert("less than 100");
+} else {
+	alert("greater than 100");
+}
+
+
+var st1 = "Denis";
+var st2 = "Dennis";
+
+if (st1 === st2) {
+	console.log("Funny");
+} else {
+	console.log("Whatever you prefer!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function name 
+function name () {
+	var name = "Denis";
+};
 
